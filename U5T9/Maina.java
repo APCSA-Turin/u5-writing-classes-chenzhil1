33825174cc6f9607af91c649f1b560228ba504e4
Chen Zhi Lin <chenzhil@nycstudents.net>
2025-01-13 14:25:04 +0000

    a

---
+++ U5T9/Maina.java
@@ -0,0 +1,39 @@
+public class Maina {
+    public static void main(String[] args) {
+        // Box b1 = new Box(10);
+        // Box b2 = new Box(20);
+        // Box b3 = new Box(10, 5, 20);
+        
+        // Box greater = b1.greaterVolume(b2);
+        // System.out.println(greater == b2);
+        // greater = b2.greaterVolume(b1);
+        // System.out.println(greater == b2);
+        // greater = b1.greaterVolume(b3);
+        // System.out.println(greater == null);
+        // greater = b3.greaterVolume(b1);
+        // System.out.println(greater == null);
+
+        // Box b1 = new Box(10);
+        // Box b2 = new Box(10, 5, 20);
+        
+        // System.out.println(b1.boxInfo());
+        // System.out.println();
+        // System.out.println(b2.boxInfo());
+
+        Box b1 = new Box(10);
+        Shape shape = new Shape();
+        b1.addToShape(shape);
+        Box shapeBox = shape.getBox();
+        System.out.println(b1 == shapeBox);
+        System.out.println(shapeBox.boxInfo());
+        System.out.println();
+        Box b2 = new Box(20, 30, 40);
+        b2.addToShape(shape);
+        shapeBox = shape.getBox();
+        System.out.println(b2 == shapeBox);
+        System.out.println(shapeBox.boxInfo());
+        
+        
+        
+    }
+}
